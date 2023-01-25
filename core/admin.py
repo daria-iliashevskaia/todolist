141fe26dea78b59e39354df856f67b110178ead3
daria-iliashevskaia <>
2023-01-25 19:37:38 +0300

    user model created, dp postgres included

---
+++ core/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    exclude = ['password']
+    readonly_fields = ('last_login', 'date_joined')
